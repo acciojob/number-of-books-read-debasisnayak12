74b6c84af2cd35fc31d58ceb73f74d83096a36b9
Debasis Nayak <115566899+debasisnayak12@users.noreply.github.com>
2023-11-19 09:12:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -18,6 +18,8 @@ const library = [
 
 const numberOfBooksRead = () => {
   // write your code here
+	let countReadBook = library.filter((book) => book.readingStatus === true);
+	return countReadBook.length;
 };
 
 // Do not change the code below
